9e6227a9a8ff8258545d84b92047606d28ce2c42
PularaPiyumali <120791863+PularaPiyumali@users.noreply.github.com>
2023-07-23 18:25:18 +0530

    New Update

--- src/app/typography/typography.component.ts
+++ src/app/typography/typography.component.ts
@@ -20,7 +20,8 @@ export class TypographyComponent implements OnInit {
   public medicine = ELEMENT_DATA;
   public dataSource: MatTableDataSource<MedicineData>;
   dataToDisplay = this.medicine;
-  searchName: '';
+  searchName: string = '';
+  matchedMedicine: any
 
   constructor (private medicineService : MedicineService, private userSearchService : UserSearchService) {
     this.dataSource = new MatTableDataSource<MedicineData>([]);
@@ -43,16 +44,53 @@ export class TypographyComponent implements OnInit {
     );
   }
 
+  // public saveSearchData(addForm: NgForm): void {
+  //   this.userSearchService.search(addForm.value).subscribe(
+  //     (response: UserSearchData) => {
+  //       console.log(response);
+  //     },
+  //     (error: HttpErrorResponse) => {
+  //       alert(error.message);
+  //       addForm.resetForm();
+  //     }
+  //   );
+  // }
+
   public saveSearchData(addForm: NgForm): void {
-    this.userSearchService.search(addForm.value).subscribe(
-      (response: UserSearchData) => {
-        console.log(response);
+    const searchValue = addForm.value?.searchName?.toLowerCase();
+    console.log(searchValue);
+    this.medicineService.getMedicine().subscribe(
+      (response: MedicineData[]) => {
+        this.medicine = response;
+        console.log(this.medicine);
+        const matchedMedicine = this.medicine.find(
+          (medicine) => medicine.medicine_name.toLowerCase() === searchValue()
+        );
+    
+        console.log(matchedMedicine);
+        if (matchedMedicine) {
+          this.userSearchService.search(addForm.value).subscribe(
+            (response: UserSearchData) => {
+              console.log(response);
+            },
+            (error: HttpErrorResponse) => {
+              alert(error.message);
+              addForm.resetForm();
+            }
+          );
+        } else {
+          
+          console.log('Medicine not found!');
+          addForm.resetForm();
+        }
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
-        addForm.resetForm();
       }
     );
+    
   }
   
+  
+
 }
