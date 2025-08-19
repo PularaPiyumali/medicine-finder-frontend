03b359001e5df7137a000c0bf6fe610dfd156ac8
PularaPiyumali <120791863+PularaPiyumali@users.noreply.github.com>
2025-08-19 22:32:09 +0530

    Update basic.auth.intercepter.service.ts

--- src/app/basic.auth.htpp.interceptor.service.ts
+++ src/app/basic.auth.htpp.interceptor.service.ts
@@ -1,30 +1,31 @@
-import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
-import { UserService } from 'app/user.service';
+import { Injectable } from "@angular/core";
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+} from "@angular/common/http";
+import { UserService } from "app/user.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class BasicAuthHtppInterceptorService implements HttpInterceptor {
-
-  constructor() { }
+  constructor() {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    if (sessionStorage.getItem('username') && sessionStorage.getItem('token')) {
-      console.log('Username:', sessionStorage.getItem('username'));
-      console.log('Token:', sessionStorage.getItem('token'));
-  
+    if (sessionStorage.getItem("username") && sessionStorage.getItem("token")) {
+      console.log("Username:", sessionStorage.getItem("username"));
+      console.log("Token:", sessionStorage.getItem("token"));
+
       req = req.clone({
         setHeaders: {
-          Authorization: sessionStorage.getItem('token')
-        }
+          Authorization: sessionStorage.getItem("token"),
+        },
       });
-  
-      console.log('Modified Request:', req);
+
+      console.log("Modified Request:", req);
     }
-  
+
     return next.handle(req);
   }
-  
-
 }
